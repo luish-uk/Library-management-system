92bcdd1d15522c6aa1e9de3c6eaabf30b3facffa
luish-uk <luishernandezlg77@gmail.com>
2026-03-24 10:34:00 +0000

    Implement add/remove/register methods

--- library.py
+++ library.py
@@ -2,11 +2,29 @@
 
 class Library:
     def __init__(self):
-        books = {}
-        members = {}
+        self.books = {}
+        self.members = {}
     
     def add_book(self, book):
-        self.books[book.isbn] = book
+        if (book.isbn) not in self.books:
+            self.books[book.isbn] = book
+        else:
+            return 'Error, this book has already been added'
+    
+    def remove_book(self, isbn):
+        if (isbn) in self.books:
+            del self.books[isbn]
+        else: 
+            return 'Error, this book doesn\'t exist'
 
     def register_member(self, member):
-        self.members[member.member_id] = member
+        if (member.member_id) not in self.members:
+            self.members[member.member_id] = member
+        else:
+            return 'Error, this member already exists'
+
+    def get_book(self, isbn):
+        return self.books[isbn]
+
+    def get_member(self, member_id):
+        return self.members[member_id]

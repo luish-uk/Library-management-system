81864cab7859ffa9068456190329c38b80c4323c
luish-uk <luishernandezlg77@gmail.com>
2026-03-24 09:38:43 +0000

    Added library class structure

--- library.py
+++ library.py
@@ -1,1 +1,12 @@
-library.py
+#NOTE: Possibly add the methods as class methods instead?
+
+class Library:
+    def __init__(self):
+        books = {}
+        members = {}
+    
+    def add_book(self, book):
+        self.books[book.isbn] = book
+
+    def register_member(self, member):
+        self.members[member.member_id] = member
